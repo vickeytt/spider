83a9009f7d6ef0e19f59f3e8343a95947db90c1d
vickeytt <40120796+vickeytt@users.noreply.github.com>
2020-10-12 12:13:32 +0800

    Add files via upload

--- spider.py
+++ spider.py
@@ -3,32 +3,57 @@ import requests
 from bs4 import BeautifulSoup
 
 lis= ['cm_guonei','cm_guonei_02','cm_guonei_03']
+num = 0
 for i in lis:
     url = 'https://temp.163.com/special/00804KVA/{}.js?callback=data_callback'.format(i)
-    print(url)
+    #print(url)
     html = requests.get(url)
     str1 = html.text.strip().replace(" ", '').replace("\n", '')
     str2 = str1.lstrip('data_callback(')
 
 
     result = str2.strip(')')
-    print(result)
-    print(type(result))
+    #print(result)
+
     # start = result.find('{"title"')
     # end = result.find('"add3":""}') + len('"add3":""}')
     data = json.loads(result)
-    print(data)
-    print(type(data))
+    #print(data)
 
     for d in data:
+        num += 1
+        print(num)
         title = d["title"]
-        print(title)
+        print('新闻标题：',title)
         source =d["source"]
-        print(source)
+        print('新闻来源：',source)
         inerurl=d['docurl']
-        print(inerurl)
+        #print(inerurl)
         html = requests.get(inerurl)
         soup = BeautifulSoup(html.text, 'lxml')
-        divs = soup.select('.HotList-item')
+        divs1 = soup.select('.post_text')
+
+        res1=''
+        for r1 in divs1:
+            res1+=r1.text.strip().replace(" ", '').replace("\n", '')
+            print('新闻内容：',res1)
+        divs2 = soup.select('.content')
+        res2 = ''
+        for r2 in divs2:
+            res2 += r2.text.strip().replace(" ", '').replace("\n", '')
+            print('新闻内容：', res2)
+
+        # with open('neteasy.txt','a',encoding='utf8') as txtfile:
+        #
+        #     txtwriter =txt.writer(txtfile,delimiter='|')
+        #
+        #     txtwriter.writerow([title,source,res1,res2])
+
+
+
+
+
+
+
 
 
